455bb866f9b7dce199573b26e386957b27acb6bc
thejjwang <jonathanjacobwang@gmail.com>
2023-07-26 16:37:57 -0700

    img loading

--- vite-project/src/CardComponent.jsx
+++ vite-project/src/CardComponent.jsx
@@ -1,16 +1,24 @@
-const CardComponent = ({avatar}) => {
+const CardComponent = ({ avatar }) => {
   return (
-    <div className="cardComponentContainer">
-      {avatar.map((avatar, i) => (
-        <div className="avatarCard" key={i}>
-          <img src={avatar.avatar} alt={avatar.id} />
-          <h3>
-            {avatar.first_name} {avatar.last_name}
-          </h3>
-          <p>{avatar.employment.title}</p>
+    // <div className="flipCard">
+    //   <div className="flipCardInner">
+        <div className="cardComponentContainer">
+          {avatar.map((avatar, i) => (
+            <div className="avatarCard" key={i}>
+              <img src={avatar.avatar} alt={avatar.id} loading="lazy"/>
+              <h3>
+                {avatar.first_name} {avatar.last_name}
+              </h3>
+              <p>{avatar.employment.title}</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+        /* <div className="flipCardBack">
+            <p>{avatar.gender}</p>
+            <p>Email: {avatar.email}</p>
+        </div> */
+    //   </div>
+    // </div>
   );
 };
 export default CardComponent;
--- vite-project/src/WorkingWithApi.jsx
+++ vite-project/src/WorkingWithApi.jsx
@@ -4,27 +4,27 @@ import CardComponent from "./CardComponent";
 const WorkingWithApi = () => {
   const [avatar, setAvatar] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://random-data-api.com/api/users/random_user?size=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      setAvatar(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://random-data-api.com/api/users/random_user?size=10",
-          {
-            method: "GET",
-          }
-        );
-        const data = await response.json();
-        setAvatar(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-  
   return (
     <div className="mainDiv">
-      <button onClick={fetchData} className="fetchRandomBtn">Fetch Random</button>
-      <CardComponent avatar={avatar}/>
+      <button onClick={fetchData} className="fetchRandomBtn">
+        Fetch Random
+      </button>
+      <CardComponent avatar={avatar} />
     </div>
   );
 };
